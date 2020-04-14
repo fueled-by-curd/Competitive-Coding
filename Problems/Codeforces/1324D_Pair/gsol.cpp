690f94ea30b76a48be32ac3b7aa5438048f6ec12
Aaditya Kumar <49308238+fueled-by-curd@users.noreply.github.com>
2020-04-14 14:57:54 +0530

    Add files via upload

---
+++ Problems/Codeforces/1324D_Pair/gsol.cpp
@@ -0,0 +1,36 @@
+#include <cstdio>
+#include <vector>
+#include <algorithm>
+typedef long long ll;
+
+int main(){
+
+    ll n; scanf("%lld", &n);
+    std::vector<ll> a(n); for(ll p = 0; p < n; p++){scanf("%lld", &a[p]);}
+    std::vector<ll> b(n); for(ll p = 0; p < n; p++){scanf("%lld", &b[p]);}
+    std::vector<ll> dp, dn; 
+    for(ll p = 0; p < n; p++){
+        ll diff = a[p] - b[p];
+        if(diff > 0){dp.push_back(diff);}
+        else{dn.push_back(diff);}
+    }
+
+    sort(dn.begin(), dn.end());
+    sort(dp.rbegin(), dp.rend());
+
+    ll lenpos = dp.size();
+    ll cnt = lenpos * (lenpos - 1) / 2;
+    //printf("%lld\n",cnt );
+    ll idx(0);
+    for(ll k = 0; k < dn.size(); k++)
+    {
+        ll cur = dn[k];
+        while((idx < dp.size()) && (cur + dp[idx] > 0)){++idx;}
+         printf("%lld\n",cnt );cnt += idx; printf("%lld\n",cnt );
+    }
+
+    printf("%lld\n", cnt);
+
+    return 0;
+}
+
