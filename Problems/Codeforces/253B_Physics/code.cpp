5874f65710c2fd9b164f64471411bcf4e6243285
Aaditya Kumar <49308238+fueled-by-curd@users.noreply.github.com>
2020-04-14 22:59:29 +0530

    Add files via upload

---
+++ Problems/Codeforces/253B_Physics/code.cpp
@@ -0,0 +1,95 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define rep(i,a,b) for(int i=a;i<b;++i)
+#define PUB push_back
+#define PUF push_front
+#define POB pop_back
+#define POF pop_front
+
+#define TRACE
+#ifdef TRACE
+#define trace1(x) cout<<#x<<"="<<x<<endl
+#define trace2(x,y) cout<<#x<<"="<<x<<" "<<#y<<"="<<y<<endl
+#define trace3(x,y,z) cout<<#x<<"="<<x<<" "<<#y<<"="<<y<<" "<<#z<<"="<<z<<endl
+#define trace4(w,x,y,z) cout<<#w<<"="<<w<<" "<<#x<<"="<<x<<" "<<#y<<"="<<y<<" "<<#z<<"="<<z<<endl
+#define tracev(a) rep(II,0,a.size()) cout<<#a<<"["<<II<<"]="<<a[II]<<" "; cout<<endl;
+#define tracea(a,n) rep(II,0,n) cout<<#a<<"["<<II<<"]="<<a[II]<<" "; cout<<endl;
+#define tracevv(a)\
+	{\
+	rep(II,0,a.size()){\
+		rep(JJ,0,a[II].size())\
+			cout<<#a<<"["<<II<<"]["<<JJ<<"]="<<a[II][JJ]<<" ";\
+		cout<<endl;\
+		}\
+	}
+#define tracem(a,r,c)\
+	{\
+	rep(II,0,r){\
+		rep(JJ,0,c)\
+			cout<<#a<<"["<<II<<"]["<<JJ<<"]="<<a[II][JJ]<<" ";\
+		cout<<endl;\
+		}\
+	}
+#else
+#define trace1(x)   
+#define trace2(x,y)   
+#define trace3(x,y,z)   
+#define trace4(w,x,y,z)  
+#define tracev(a) 
+#define tracea(a,n)  
+#define tracevv(a)  
+#define tracem(a,r,c)  
+#endif
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef vector<vector<int> > vvi;
+typedef vector<vector<ll> > vvll;
+typedef pair<int,int> pii;
+
+int main()
+{
+	ios::sync_with_stdio(0);
+
+	int n;
+	
+	cin >> n;
+
+	//trace2(l1,l2);
+	vi v(n);
+	int i=0;
+	int inp = 0;
+	
+	while(i < n && cin >> inp)
+		v[i++] = inp;
+
+	sort(v.begin(),v.end());
+
+	int l=0;
+	int r=n-1;
+	int a=0;
+	int b=0;
+	while(l <= r)
+	{
+
+		if(v[r] > 2*v[l]  )
+		{
+			a = v[r-1];
+			b = v[l+1];
+			if( a> 2*v[l])
+				l++;
+			else if(v[r] > 2*b)
+				r--;
+		}
+	
+		else
+			break;
+	}
+
+	int ans = n-(r-l+1);
+	cout << ans  << "\n";
+	return 0;
+}
