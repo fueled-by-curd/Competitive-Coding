85f507a64f4dacc495f565dab56f0b424c744fd7
Aaditya Kumar <49308238+fueled-by-curd@users.noreply.github.com>
2020-07-09 22:33:55 +0530

    Create LastStone_II.cpp

---
+++ Problems/LeetCode/Medium/LastStone_II.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int lastStoneWeightII(vector<int>& stones) 
+    {
+        if( stones.empty())
+            return 0;
+        
+        int sum = accumulate(stones.begin(),stones.end(),0);
+        int S = sum;
+        sum /= 2;
+        int n = stones.size();
+        vector<vector<int> > dp(n +1 ,vector<int>(sum+1,0));
+        
+        for( int i = 1; i <= n; i++)
+        {
+            for( int j = 0; j <= sum; j++)
+            {
+                if( j >= stones[i-1])
+                    dp[i][j] = max( dp[i-1][j], dp[i-1][j-stones[i-1]] + stones[i-1]);
+                else
+                    dp[i][j] = dp[i-1][j];
+            }
+        }
+        
+        int S2 = dp[n][sum];
+        
+        return S-2*S2;
+        
+        
+    }
+};
